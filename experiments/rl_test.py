6f1e6263dd843a6ab3389e2185d32fcee3285b3b
Fabian <fabianmoraesb@gmail.com>
2026-02-21 17:34:04 +0100

    created choose_move function and q_table

--- experiments/rl_test.py
+++ experiments/rl_test.py
@@ -35,42 +35,71 @@ def boundaries(pos, length) -> int:
 
     return pos 
 
-def print_map(world,  pos, end):
+def print_map(world: list,  pos: int, end: int):
     print("----------------------------------------------")
     result = ""
-    for i, el in enumerate(world):
-        if i == end:
-            result += "M"
+    for i, el in enumerate(world): 
         if i==pos:
             result+="P"
+        elif i == end:
+            result += "M"
         else:
             result+="0"
     print(result)
     print("-----------------------------------------------")
 
+def bellman_equation(learning_rate: float, eps: float, reward:int, pos: int):
+    ...
+
+def choose_move(pos: int, q_table: list, eps: float) -> int:
+    if random.uniform(0,1) < eps:
+        return random.choice([0,1])
+    
+    if q_table[pos][0] < q_table[pos][1]:
+        return -1
+    else:
+        return 1
+
 def main():
     world = [0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,2,0,0,0,0,0]
     length = len(world)
+    q_table = [[0.0, 0.0] for _ in range(length)]
     start, end = init(world)
     total_score = 0
     pos = start
+    eps = 1.0
+    eps_rate = 0.98
+    eps_min = 0.01
+    lr = 0.1
 
-    while True:
-        move = input("key: ")
-        if move == 'a':
-            prev_pos = pos
-            pos -= 1
-            pos = boundaries(pos, length)
-            total_score+=rewards(pos,end,prev_pos)
+    for epoch in range(50):
 
-        elif move == 'd':
-            prev_pos = pos
-            pos+=1
-            pos = boundaries(pos,length)                          
-            total_score+=rewards(pos,end,prev_pos)
+        while True:
+
+ 
+            print_map(world,pos,end)
+            move = choose_move(pos, q_table, eps)            
+            if move == 'a':
+                prev_pos = pos
+                pos -= 1
+                pos = boundaries(pos, length)
+                total_score+=rewards(pos,end,prev_pos)
+
+            elif move == 'd':
+                prev_pos = pos
+                pos+=1
+                pos = boundaries(pos,length)                          
+                total_score+=rewards(pos,end,prev_pos)
+
+            if pos == end:
+                break
+
+            if move == 'q':
+                break
+        
+        eps *= eps_rate
         print(total_score)
-        if move == 'q':
-            break
+    
 
 if __name__ == "__main__": 
     main()
