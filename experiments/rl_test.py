2925a42f0750d6c7f4a1ba618c4985b78509c77e
Fabian <fabianmoraesb@gmail.com>
2026-02-21 16:17:51 +0100

    created (temporary) loop for user to move in rl_test

--- experiments/rl_test.py
+++ experiments/rl_test.py
@@ -1,34 +1,63 @@
 import time, random
 
-world = [0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,2,0,0,0,0,0]
+def init(world) -> tuple:
+    for i in range(len(world)):
+        if world[i]==1:
+            start = i
+        if world[i]==2:
+            end = i
+    
+    return (start, end)
 
-for i in world:
-    if i==1:
-        start = i
-    if i==2:
-        end = i
 
 def rewards(pos: int,end: int,  prev_pos: int) -> int:
     
     if pos == end:
-        score+=100
+        return 100
 
     elif abs(end-pos) > abs(end-prev_pos):
-        score -= 10
+        return -10
 
     elif abs(end-pos) < abs(end-prev_pos):
-        score += 10
+        return 10
 
     elif pos == prev_pos:
-        score -= 30 
-    return score
+        return -30 
 
+
+def boundaries(pos, length) -> int:
+    
+    if pos<0:
+        pos = 0
+
+    if pos>length-1:
+        pos = length-1
+
+    return pos 
 
 def main():
-     
-    ...
-          
-        
+    world = [0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,2,0,0,0,0,0]
+    length = len(world)
+    start, end = init(world)
+    total_score = 0
+    pos = start
+
+    while True:
+        move = input("key: ")
+        if move == 'a':
+            prev_pos = pos
+            pos -= 1
+            pos = boundaries(pos, length)
+            total_score+=rewards(pos,end,prev_pos)
+
+        elif move == 'd':
+            prev_pos = pos
+            pos+=1
+            pos = boundaries(pos,length)                          
+            total_score+=rewards(pos,end,prev_pos)
+        print(total_score)
+        if move == 'q':
+            break
 
 if __name__ == "__main__": 
     main()
